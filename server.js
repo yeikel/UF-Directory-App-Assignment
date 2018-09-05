5c7327be458ecb08e4c4486ccff8ab16f950bb41
yeikel <email@yeikel.com>
2018-09-05 01:07:37 -0400

    Remove unnecessary code

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ http.createServer((req, res) => {
         res.writeHead(404, {'Content-Type': 'application/text'});
         res.end("Bad gateway error");
     }
-    res.end();
-
+    
 }).listen(port, '127.0.0.1');
 
 console.log('Server listening on: http://127.0.0.1:' + port);
