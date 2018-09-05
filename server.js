9b5e34c0c1a977abf1c0e1f67f859c13e4b6a87d
yeikel <email@yeikel.com>
2018-09-05 01:11:05 -0400

    Remove redundant else statement

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(listings));
         return;
-    }else{
-        res.writeHead(404, {'Content-Type': 'application/text'});
-        res.end("Bad gateway error");
     }
     
+    res.writeHead(404, {'Content-Type': 'application/text'});
+    res.end("Bad gateway error");
+
 }).listen(port, '127.0.0.1');
 
 console.log('Server listening on: http://127.0.0.1:' + port);
